4130dccc2c6494f176a20039588a9593cccea269
linjinping <58460142+linjinping@users.noreply.github.com>
2020-06-02 20:17:55 +0800

    Add files via upload

---
+++ E2_3424/Program.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.IO.Ports;
+using System.Threading;
+
+public class PortChat
+{
+    static bool _continue;
+    static SerialPort Serialport;
+
+
+    public static void Main()
+    {        
+        string name;
+        string message;
+        Serialport = new SerialPort();
+        Serialport.ReadTimeout = 500;
+        Serialport.WriteTimeout = 500;
+        StringComparer stringComparer = StringComparer.OrdinalIgnoreCase;
+        Thread readThread = new Thread(Read);
+
+        //分别获取:名字 波特率 校验位 数据位 停止位数量
+        Serialport.PortName = SetPortName(Serialport.PortName);
+        Serialport.BaudRate = SetPortBaudRate(Serialport.BaudRate);
+        Serialport.Parity = SetPortParity(Serialport.Parity);
+        Serialport.DataBits = SetPortDataBits(Serialport.DataBits);
+        Serialport.StopBits = SetPortStopBits(Serialport.StopBits);
+        Serialport.Handshake = SetPortHandshake(Serialport.Handshake);
+
+        //设置完信息后打开端口
+        Serialport.Open();
+        _continue = true;
+       
+        readThread.Start();
+
+
+        Console.Write("Name: ");
+        name = Console.ReadLine();
+
+        Console.WriteLine("Type QUIT to exit");
+
+        while (_continue)
+        {
+            
+            message = Console.ReadLine();
+
+            if (stringComparer.Equals("quit", message))
+            {
+                _continue = false;
+            }
+            else
+            {
+                Console.WriteLine("["+DateTime.Now.ToString("yyyy/MM/dd h:mm:ss.fff") +" SEND]"+name+" SEND: "+message);
+                Serialport.WriteLine(
+                     
+                String.Format("{0} SEND: {1}", name, message));
+            }
+        }
+
+        readThread.Join();
+        Serialport.Close();
+    }
+
+    public static void Read()
+    {
+        while (_continue)
+        {
+            try
+            {
+                string message = Serialport.ReadLine();
+                Console.WriteLine("["+DateTime.Now.ToString("yyyy/MM/dd h:mm:ss.fff") +" RECV]"+message);
+            }
+            catch (TimeoutException) { }
+        }
+    }
+
+    // Display Port values and prompt user to enter a port.
+    public static string SetPortName(string defaultPortName)
+    {
+        string portName;
+
+        Console.WriteLine("Available Ports:");
+        foreach (string s in SerialPort.GetPortNames())
+        {
+            Console.WriteLine("   {0}", s);
+        }
+
+        Console.Write("Enter COM port value (Default: {0}): ", defaultPortName);
+        portName = Console.ReadLine();
+
+        if (portName == "" || !(portName.ToLower()).StartsWith("com"))
+        {
+            portName = defaultPortName;
+        }
+        return portName;
+    }
+    // Display BaudRate values and prompt user to enter a value.
+    public static int SetPortBaudRate(int defaultPortBaudRate)
+    {
+        string baudRate;
+
+        Console.Write("Baud Rate(default:{0}): ", defaultPortBaudRate);
+        baudRate = Console.ReadLine();
+
+        if (baudRate == "")
+        {
+            baudRate = defaultPortBaudRate.ToString();
+        }
+
+        return int.Parse(baudRate);
+    }
+
+    // Display PortParity values and prompt user to enter a value.
+    public static Parity SetPortParity(Parity defaultPortParity)
+    {
+        string parity;
+
+        Console.WriteLine("Available Parity options:");
+        foreach (string s in Enum.GetNames(typeof(Parity)))
+        {
+            Console.WriteLine("   {0}", s);
+        }
+
+        Console.Write("Enter Parity value (Default: {0}):", defaultPortParity.ToString(), true);
+        parity = Console.ReadLine();
+
+        if (parity == "")
+        {
+            parity = defaultPortParity.ToString();
+        }
+
+        return (Parity)Enum.Parse(typeof(Parity), parity, true);
+    }
+    // Display DataBits values and prompt user to enter a value.
+    public static int SetPortDataBits(int defaultPortDataBits)
+    {
+        string dataBits;
+
+        Console.Write("Enter DataBits value (Default: {0}): ", defaultPortDataBits);
+        dataBits = Console.ReadLine();
+
+        if (dataBits == "")
+        {
+            dataBits = defaultPortDataBits.ToString();
+        }
+
+        return int.Parse(dataBits.ToUpperInvariant());
+    }
+
+    // Display StopBits values and prompt user to enter a value.
+    public static StopBits SetPortStopBits(StopBits defaultPortStopBits)
+    {
+        string stopBits;
+
+        Console.WriteLine("Available StopBits options:");
+        foreach (string s in Enum.GetNames(typeof(StopBits)))
+        {
+            Console.WriteLine("   {0}", s);
+        }
+
+        Console.Write("Enter StopBits value (None is not supported and \n" +
+         "raises an ArgumentOutOfRangeException. \n (Default: {0}):", defaultPortStopBits.ToString());
+        stopBits = Console.ReadLine();
+
+        if (stopBits == "")
+        {
+            stopBits = defaultPortStopBits.ToString();
+        }
+
+        return (StopBits)Enum.Parse(typeof(StopBits), stopBits, true);
+    }
+    public static Handshake SetPortHandshake(Handshake defaultPortHandshake)
+    {
+        string handshake;
+
+        Console.WriteLine("Available Handshake options:");
+        foreach (string s in Enum.GetNames(typeof(Handshake)))
+        {
+            Console.WriteLine("   {0}", s);
+        }
+
+        Console.Write("Enter Handshake value (Default: {0}):", defaultPortHandshake.ToString());
+        handshake = Console.ReadLine();
+
+        if (handshake == "")
+        {
+            handshake = defaultPortHandshake.ToString();
+        }
+
+        return (Handshake)Enum.Parse(typeof(Handshake), handshake, true);
+    }
+}
